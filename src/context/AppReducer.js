af368436ea498ddfa0f06742bac463d84240055f
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-09-08 04:15:00 +0700

    app reducer to integrate global state

---
+++ src/context/AppReducer.js
@@ -0,0 +1,33 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default (state, action) => {
+  switch (action.type) {
+    case "REMOVE_EMPLOYEE":
+      return {
+        ...state,
+        employees: state.employees.filter(
+          (employee) => employee.id !== action.payload
+        )
+      };
+    case "ADD_PLAYER":
+      return {
+        player: action.payload
+      };
+    case "GET_ALL_ITEMS":
+      return {
+        ...state,
+        items: [action.payload]
+      };
+    case "ADD_ITEMS":
+      return {
+        ...state,
+        items: [...state.items, action.payload]
+      };
+    case "TRANSFER_NFT":
+      return {
+        ...state,
+        transactions: [...state.transactions, action.payload]
+      };
+    default:
+      return state;
+  }
+};
