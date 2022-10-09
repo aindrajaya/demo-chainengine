53e57193a5e3f997d7f1dc350a97691067281bb5
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-10-10 05:41:00 +0700

    update heading component

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -110,6 +110,52 @@ export const Heading = () => {
           </button>
         </Link>
       </div>
+      {/* <div className="flex-grow text-right px-4 py-2 m-2">
+        <Link to="/upload">
+          <button className="bg-green-400 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-plus-circle"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <line x1="12" y1="8" x2="12" y2="16"></line>
+              <line x1="8" y1="12" x2="16" y2="12"></line>
+            </svg>
+            <span className="pl-2">Check upload</span>
+          </button>
+        </Link>
+      </div> */}
+      <div className="flex-grow text-right px-4 py-2 m-2">
+        <Link to="/listing">
+          <button className="bg-green-400 hover:bg-green-500 text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-plus-circle"
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <line x1="12" y1="8" x2="12" y2="16"></line>
+              <line x1="8" y1="12" x2="16" y2="12"></line>
+            </svg>
+            <span className="pl-2">Check listing</span>
+          </button>
+        </Link>
+      </div>
       <h2 className="font-medium leading-tight text-2xl mt-0 mb-6 text-black-600">
         Player List (<span className="text-gray-600">ID and Wallet Address</span>):{" "}
       </h2>
