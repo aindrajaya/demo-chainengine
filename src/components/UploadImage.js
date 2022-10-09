48f237591a4369bb7c1e35c3436cfff1810fcac9
Arista Indrajaya <arista.indrajaya@gmail.com>
2022-10-10 05:45:00 +0700

    upload feature, not fixed errror

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,26 +1,30 @@
 import React, { useState } from 'react'
 import {sdk} from "../services";
-import { createReadStream } from 'fs';
+import fs from 'fs';
 import { useHistory } from "react-router-dom";
 
 export default function UploadImage() {
   const history = useHistory()
-  const fileReadStream = createReadStream(`./images/shield.jpg`);
+  const fileReadStream = fs.createReadStream('./images/shield.jpg');
   console.log(fileReadStream, "data Image")
   const [uploadSuccess, setUploadSuccess] = useState([])
 
   const uploadResponse = async (file) => {
-    const data = await sdk.nfts.uploadFileToIFPS(file);
-    setUploadSuccess(data)
-    // history.push("/")
+    await sdk.nfts.uploadFileToIFPS(file)
+      .then((data) => {
+        setUploadSuccess(data)
+      })
+      .catch((e) => {
+        alert(e)
+      })
+    history.push("/")
   }
-
-  console.log(uploadSuccess, "data upload sukses")
 
   // console.log(uploadResponse(fileReadStream), "data terupload");
   return (
     <div>
       <button onClick={() => uploadResponse(fileReadStream)}></button>
+      <h1>{uploadSuccess.data?.url}</h1>
     </div>
   )
 }
